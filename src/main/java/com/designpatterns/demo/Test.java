d81fdabaae4d31294a129d099edf13c0356af8ce
cm <chenmeng_1919@163.com>
2021-08-01 12:08:19 +0800

    本地新增

---
+++ src/main/java/com/designpatterns/demo/Test.java
@@ -0,0 +1,12 @@
+package com.designpatterns.demo;
+
+/*
+ * @author: cm
+ * @date: Created in 2021/8/1 12:06
+ * @description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        
+    }
+}
