c049e7b7247a69bed40f91fbbcdc7c61249a90bb
cm <chenmeng_1919@163.com>
2021-08-16 18:21:21 +0800

    传输对象模式-Transfer Object Pattern

---
+++ src/main/java/com/designpatterns/demo/TransferObjectPattern/TransferObjectPatternDemo.java
@@ -0,0 +1,28 @@
+package com.designpatterns.demo.TransferObjectPattern;
+
+/*
+ * @author: cm
+ * @date: Created in 2021/8/16 18:16
+ * @description:使用 StudentBO 来演示传输对象设计模式
+ */
+public class TransferObjectPatternDemo {
+    public static void main(String[] args) {
+        StudentBO studentBusinessObject = new StudentBO();
+
+        //输出所有的学生
+        for (StudentVO student : studentBusinessObject.getAllStudents()) {
+            System.out.println("Student: [RollNo : "
+                    + student.getRollNo() + ", Name : " + student.getName() + " ]");
+        }
+
+        //更新学生
+        StudentVO student = studentBusinessObject.getAllStudents().get(0);
+        student.setName("Michael");
+        studentBusinessObject.updateStudent(student);
+
+        //获取学生
+        studentBusinessObject.getStudent(0);
+        System.out.println("Student: [RollNo : "
+                + student.getRollNo() + ", Name : " + student.getName() + " ]");
+    }
+}
